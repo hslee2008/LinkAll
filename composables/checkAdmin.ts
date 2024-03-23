d0ccef3cf33c0ea5609ea70c81ab536c864bec9c
Hyunseung <hyunseunglee2008@gmail.com>
2024-03-23 17:28:53 +0900

    v1.0.10

--- composables/checkAdmin.ts
+++ composables/checkAdmin.ts
@@ -1,4 +1,8 @@
 export default function checkAdmin(email: string): Boolean {
-  const admins = ["h.junho420@gmail.com", "hyunseunglee2008@gmail.com"];
+  const admins = [
+    "h.junho420@gmail.com",
+    "hyunseunglee2008@gmail.com",
+    "kimhyunhee414@gmail.com",
+  ];
   return admins.indexOf(email) !== -1;
 }
